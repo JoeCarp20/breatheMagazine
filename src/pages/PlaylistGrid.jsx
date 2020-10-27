28d786bbbe011581f08b824a1e40ee62e1f88fe1
Joe Boylson <jboylson@saec-kv.com>
2020-10-27 18:29:33 -0400

    styling playlist grid and playlist player;

--- src/pages/PlaylistGrid.jsx
+++ src/pages/PlaylistGrid.jsx
@@ -45,24 +45,30 @@ const PlaylistCard = ({name, cover, description, creator, spotify_id}) => {
     const { setPlaylistId } = useContext(PlaylistContext);
 
     return (
-        <div className={'playlist-card grid-2'}>
+        <div className={'playlist-card'}>
 
-            <div className={'playlist-card-cover'}>
-                <Image fluid={cover}/>
-                <div className={'cover-overlay'}></div>
-            </div>
+            <Image fluid={cover}/>
 
             <div className={'playlist-card-body'}>
-                <p className={'emph'}>{name}</p>
 
-                { description ? (
-                    <PortableText blocks={description}/>
-                ) : (
-                    <p>A playlist by {creator ? creator.name : `an anonymous creator`}</p>
-                )}
+                <div className={'name'}>
+                    <p className={'emph'}>{name}</p>
+                </div>
+                
+                <div className={'description'}>
+                    { description ? (
+                        <PortableText blocks={description} className={'description'}/>
+                    ) : (
+                        <p>A playlist by {creator ? creator.name : `an anonymous creator`}</p>
+                    )}
+                </div>
 
-                { creator && <Link to={`/creators/${creator.slug.current}`}>By {creator.name}</Link> }
-                { spotify_id && <p onClick={() => setPlaylistId(spotify_id)}>Play {name || 'Unnamed Playlist'}</p> }
+                <div className={'controls'}>
+                    { creator && <Link to={`/creators/${creator.slug.current}`}>By {creator.name}</Link> }
+                    { spotify_id && <p onClick={() => setPlaylistId(spotify_id)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 8.64L15.27 12 10 15.36V8.64M8 5v14l11-7L8 5z"/></svg>
+                    </p> }
+                </div>
 
             </div>
 
